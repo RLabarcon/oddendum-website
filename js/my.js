178c54b74ceac4ce33c692b902c5f9f83c5b43ff
Ravenstorm Labarcon <ravenstormmlabarcon@gmail.com>
2015-11-30 18:36:33 -0800

    none

--- js/my.js
+++ js/my.js
@@ -3,10 +3,17 @@ $(document).ready(function() {
 	var $window = $(window);
 
 	function boxAdjust() {
+<<<<<<< HEAD
 		if (860 < $window.width() < 1280) {
 			$('.article-link').removeClass('full-width').removeClass('third-width').addClass('half-width');
 		}
 		if ($window.width() < 860) {
+=======
+		if (850 < $window.width() < 1280) {
+			$('.article-link').removeClass('full-width').removeClass('third-width').addClass('half-width');
+		}
+		if ($window.width() < 850) {
+>>>>>>> parent of c37e3bd... 2.0.4
 			$('.article-link').removeClass('half-width').removeClass('third-width').addClass('full-width');
 		}
 		if ($window.width() > 1560) {
