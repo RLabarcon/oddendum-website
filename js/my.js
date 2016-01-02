ec8e1844e1f9429d3647d6a842225900d10f5e4e
Ravenstorm Labarcon <ravenstormmlabarcon@gmail.com>
2016-01-01 20:29:48 -0800

    2016

--- js/my.js
+++ js/my.js
@@ -1,20 +1,16 @@
 $(document).ready(function() {
-	
-	$('.article-display').hover(function() {
-		$('.article-display overlay').css('opacity', '0.3');
-	});
 
 	var $window = $(window);
 
 	function boxAdjust() {
 		if (860 < $window.width() < 1280) {
-			$('.article-link').removeClass('full-width').removeClass('third-width').addClass('half-width');
+			$('.tile').removeClass('full-width').removeClass('third-width').addClass('half-width');
 		}
 		if ($window.width() < 860) {
-			$('.article-link').removeClass('half-width').removeClass('third-width').addClass('full-width');
+			$('.tile').removeClass('half-width').removeClass('third-width').addClass('full-width');
 		}
 		if ($window.width() > 1560) {
-			$('.article-link').removeClass('full-width').removeClass('half-width').addClass('third-width');
+			$('.tile').removeClass('full-width').removeClass('half-width').addClass('third-width');
 		}
 	}
 
