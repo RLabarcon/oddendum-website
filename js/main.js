34c7472d46ffaa1b17dfb9cceae1ed5636a7300f
Ravenstorm Labarcon <ravenstormmlabarcon@gmail.com>
2016-01-04 01:49:09 -0800

    Konami Plus
    Added a jquery function that changes the color and assigns the active
    class while scrolling.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
 
+    if (scroll < 1500 && scroll > 500) {
+    	$("#heist").addClass("active")
+        $(".text-holder").addClass("james");
+    } 
+    else {
+    	$("#heist").removeClass("active")
+        $(".text-holder").removeClass("james");
+    }
+
+    if (scroll >= 1500) {
+    	$("#passion").addClass("active")
+        $(".text-holder").addClass("max");
+    } 
+    else {
+    	$("#passion").removeClass("active")
+        $(".text-holder").removeClass("max");
+    }
+});
